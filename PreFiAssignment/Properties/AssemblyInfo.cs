e9f87a384032b4216b8282298300239537c4b04e
kuukolol <alarcon.321748@cubao.sti.edu.ph>
2025-04-10 22:03:52 +0800

    update added author name

--- PreFiAssignment/Properties/AssemblyInfo.cs
+++ PreFiAssignment/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PreFiAssignment")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Created by Dr.House (shinonome, kuukolol, kao)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nioters")]
 [assembly: AssemblyProduct("PreFiAssignment")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
