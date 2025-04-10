58fb815a7006575c095e819a691c98f1a5af8514
kuukolol <alarcon.321748@cubao.sti.edu.ph>
2025-04-10 22:01:22 +0800

    Add project files.

---
+++ PreFiAssignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreFiAssignment
+{
+    public class MyNameinArray
+    {
+        public void DisplayName()
+        {
+            string fullName = "SHINONOME KUUKO KAONASHI";
+            fullName = fullName.Replace(" ", "");
+
+            int cols = 5;
+            int rows = (int)Math.Ceiling(fullName.Length / (double)cols);
+
+            int index = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (index < fullName.Length)
+                    {
+                        
+                        Console.Write(fullName[index] + " ");
+                        index++;
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyNameinArray myName = new MyNameinArray();
+            myName.DisplayName();
+        }
+    }
+}
